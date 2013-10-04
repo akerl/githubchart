8d80ae8cce2f736f9c55572949c49b5361b66901
Les Aker <me@lesaker.org>
2013-10-03 23:45:49 -0400

    rasem looks interesting

--- plot.rb
+++ plot.rb
@@ -2,6 +2,7 @@
 
 require 'github_stats'
 require 'matrix'
+require 'rasem'
 
 Colors = ['#eeeeee', '#d6e685', '#8cc665', '#44a340', '#1e6823']
 
@@ -11,4 +12,8 @@ data.first.date.wday.times { data.unshift Github_Stats::Datapoint.new(data.first
 (6 - data.last.date.wday).times { data << Github_Stats::Datapoint.new(data.last.date.next, -1) }
 
 Grid = Matrix[*data.each_slice(7).to_a.transpose]
+
+img = Rasem::SVGImage.new(10 * Grid.row_count, 10 * Grid.column_count) do
+    # stuff
+end
 
