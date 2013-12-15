f994979f5b6c879492f79f451653c1e20cf3c0f9
Les Aker <me@lesaker.org>
2013-12-15 09:48:04 -0500

    test svg

---
+++ spec/githubchart/svg_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require 'tempfile'
+
+describe GithubChart::Chart do
+  let(:svg) { GithubChart.new.svg }
+
+  before(:all) do
+    @file = Tempfile.new('svg')
+    @file.write(GithubChart.new.svg)
+    @path = @file.path
+    @file.close
+  end
+
+  after(:all) { @file.unlink }
+
+  describe '.svg' do
+    it 'makes an SVG' do
+      expect(`file #{@path}`).to include('Scalable Vector Graphic')
+    end
+  end
+end
